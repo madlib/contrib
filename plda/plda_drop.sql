230eff36fa900bb47dfc59f1450f2c83c4d27d7f
Kee Siong Ng <keesiong.ng@emc.com>
2011-02-13 17:23:33 +1100

    Minor changes to function signatures

--- plda/plda_drop.sql
+++ plda/plda_drop.sql
@@ -2,7 +2,7 @@ DROP TYPE IF EXISTS madlib.topics_t CASCADE;
 DROP TABLE IF EXISTS madlib.lda_corpus CASCADE;
 DROP TABLE IF EXISTS madlib.lda_dict CASCADE; 
 DROP TYPE IF EXISTS madlib.global_counts CASCADE;
-DROP AGGREGATE IF EXISTS madlib.cword_agg(int4[], int[], int, int);
+DROP AGGREGATE IF EXISTS madlib.cword_agg(int4[], int4[], int4, int4, int4);
 DROP TABLE IF EXISTS madlib.localWordTopicCount CASCADE;
 DROP TABLE IF EXISTS madlib.globalWordTopicCount CASCADE;
 -- DROP AGGREGATE IF EXISTS madlib.sum2darrays(madlib.global_counts, int, int);
