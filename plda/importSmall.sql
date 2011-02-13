dac57fe7cde80857a3bcdd30ea4e8cc97aed543e
Kee Siong Ng <keesiong.ng@emc.com>
2011-02-13 17:21:25 +1100

    Changed test data files to reside in new tables.

--- plda/importSmall.sql
+++ plda/importSmall.sql
@@ -3,8 +3,10 @@
 -- Can use R's lda.lexicalize() function to convert texts into the 
 -- desired format.
 
-INSERT INTO madlib.corpus VALUES 
- (1, '{1,2,3,4,5,6,7}'), -- '{Human,machine,interface,for,ABC,computer,applications}'),
+DROP TABLE IF EXISTS madlib.mycorpus;
+CREATE TABLE madlib.mycorpus ( id int4, contents int4[] ) DISTRIBUTED BY (id);
+INSERT INTO madlib.mycorpus VALUES 
+ (1, '{1,2,3,4,5,6,7}'), 
  (2, '{8,9,10,11,12,10,6,13,14,15}'),
  (3, '{16,17,11,3,18,13}'),
  (4, '{13,19,1,13,20,21,10,17}'),
@@ -14,9 +16,10 @@ INSERT INTO madlib.corpus VALUES
  (8, '{32,35,36,37,10,30,19,38,39,29}'),
  (9, '{32,35,8,9}') ;
 
-insert into madlib.lda_dict values
- (1000000, 
-  '{human,machine,interface,for,abc,computer,applications,a,survey,of,
+DROP TABLE IF EXISTS madlib.mydict;
+CREATE TABLE madlib.mydict ( dict text[] ) DISTRIBUTED RANDOMLY;
+insert into madlib.mydict values
+ ('{human,machine,interface,for,abc,computer,applications,a,survey,of,
     user,opinion,system,response,time,the,eps,management,and,engineering,
     testing,relation,perceived,to,error,generation,random,binary,order,tree,
     intersection,graph,path,in,minor,IV,widths,well,quasi}');
