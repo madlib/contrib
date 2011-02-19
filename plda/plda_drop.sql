1d12776be28fbf591058000b8bce69f3c4363d4e
Kee Siong Ng <keesiong.ng@emc.com>
2011-02-19 23:15:58 +1100

    lda_testcorpus drop

--- plda/plda_drop.sql
+++ plda/plda_drop.sql
@@ -1,5 +1,6 @@
 DROP TYPE IF EXISTS madlib.topics_t CASCADE;
 DROP TABLE IF EXISTS madlib.lda_corpus CASCADE;
+DROP TABLE IF EXISTS madlib.lda_testcorpus CASCADE;
 DROP TABLE IF EXISTS madlib.lda_dict CASCADE; 
 DROP TYPE IF EXISTS madlib.global_counts CASCADE;
 DROP AGGREGATE IF EXISTS madlib.cword_agg(int4[], int4[], int4, int4, int4);
