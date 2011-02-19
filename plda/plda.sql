02848af5a344034236b2c34210017e4c2ca36dbb
Kee Siong Ng <keesiong.ng@emc.com>
2011-02-19 23:15:26 +1100

    Added functions for labelling new documents. Minor changes to doxygen doc.

--- plda/plda.sql
+++ plda/plda.sql
@@ -21,18 +21,18 @@ model for natural texts that has received a lot of attention in recent years.
 The model is quite versatile, having found uses in problems like automated 
 topic discovery, collaborative filtering, and document classification.
 
-The LDA model posits that each document has a mixture of various topics (e.g.
-a document is related to Topic 1 with probability 0.7, and Topic 2 with 
+The LDA model posits that each document is associated with a mixture of various 
+topics (e.g. a document is related to Topic 1 with probability 0.7, and Topic 2 with 
 probability 0.3), and that each word in the document is attributable to one 
 of the document's topics. There is a (symmetric) Dirichlet prior with parameter 
 \f$ \alpha \f$ on each document's topic mixture. In addition, there is another 
 (symmateric) Dirichlet prior with parameter \f$ \eta \f$ on the distribution 
 of words for each topic. The following generative process then defines a distribution 
-over a corpus of documents. First sample a per-topic word distribution 
-\f$ \Phi_i \f$, for each topic \f$ i \f$, from the \f$ Dirichlet(\eta) \f$ prior. 
+over a corpus of documents. First sample, for each topic \f$ i \f$, a per-topic word distribution 
+\f$ \Phi_i \f$ from the Dirichlet(\f$\eta\f$) prior. 
 Then for each document:
 -# Sample a document length N from a suitable distribution, say, Poisson.
--# Sample a topic mixture \f$ \theta \f$ for the document from the \f$ Dirichlet(\alpha) \f$ distribution.
+-# Sample a topic mixture \f$ \theta \f$ for the document from the Dirichlet(\f$\alpha\f$) distribution.
 -# For each of the N words:
    -# Sample a topic \f$ z_n \f$ from the multinomial topic distribution \f$ \theta \f$.
    -# Sample a word \f$ w_n \f$ from the multinomial word distribution \f$ \Phi_{z_n} \f$ associated with topic \f$ z_n \f$.
@@ -49,18 +49,31 @@ a straightforward parallelisation of the Gibbs sampling algorithm.
 
 See also http://code.google.com/p/plda/.
 
-@prereq
-
-None 
+@installation
+   To install the module, run the following command in the plda source directory to compile 
+   the shared object file.
+   \code
+   make install
+   \endcode
+   If the machine is a multi-node machine, we need to copy plda_support.so to all the segment
+   hosts as follows (you may need to do this as gpadmin):
+   \code
+   gpscp -f /path/to/host_file plda_support.so =:$GPHOME/lib/postgresql/
+   \endcode
+   We can then install the parallel LDA user-defined functions as follows:
+   \code
+   psql db_name -f plda.sql
+   \endcode
 
 @usage
 
-Here is the main learning function.
+This is the main learning function.
 
 -  Topic inference is achieved through the following Python function
    \code
-   plda_run(datatable, dicttable, numiter int, numtopics int, alpha float8, eta float8, restart bool)
-   \endcode,
+   plda_run(datatable, dicttable, numiter int, numtopics int, 
+            alpha float8, eta float8, restart bool),
+   \endcode
    where datatable is the table where the corpus to be analysed is stored (the table is
    assumed to have the two columns: id int and contents int[]), dicttable is the table
    where the dictionary is stored (the table is assumed to have the column dict text[],
@@ -71,78 +84,77 @@ Here is the main learning function.
    terminated inference run. The plda_run() function needs to be run from within
    Python.
 
-@installation
-   In the plda source directory, run the following commands:
-   \code
-   make install
-   psql db_name -f plda.sql
-   \endcode
 
 @examp
+
+We now give a usage example.
 
 -# As a first step, we need to prepare and populate two tables/views with the following 
    structure:
    \code   
-        corpus_table_name (       
-                id         INT4,    -- document ID
-                contents   INT4[],  -- words in the document
-		....               -- other fields
-    	);
+   corpus_table_name (       
+         id         INT4,    -- document ID
+         contents   INT4[],  -- words in the document
+	 ....                -- other fields
+   );
    \endcode
    and
    \code
-	dictionary_table_name (
-		a          TEXT[]  -- array of words in the dictionary
-        );
+   dictionary_table_name (
+	 a          TEXT[]  -- array of words in the dictionary
+   );
    \endcode
    The module comes with some randomly generated data. For example, we can import two
    test tables madlib.mycorpus and madlib.mydict using the command
    \code
-        psql database_name -f importTestcases.sql
-   \endcode
-     
--# We can now kick-start the inference process by running the following inside a
-   Python session:
+   psql database_name -f importTestcases.sql
+   \endcode     
+-# We can now kick-start the inference process by running the following.
    \code
-        import plda
-        plda.plda_run('madlib.mycorpus', 'madlib.mydict', 20, 10, 0.5, 0.5, False)
+   python
+   >>> import plda
+   >>> plda.plda_run('madlib.mycorpus', 'madlib.mydict', 20,10,0.5,0.5,False)
    \endcode
    If the program terminates without converging to a good solution, we can restart 
    the learning process where it terminated by running more iterations as follows: 
    \code
-        plda.plda_run('madlib.mycorpus', 'madlib.mydict', 50, 10, 0.5, 0.5, True)
+   >>> plda.plda_run('madlib.mycorpus', 'madlib.mydict', 50,10,0.5,0.5,True)
    \endcode
-   This restarting process can be run multiple times.
- 
+   This restarting process can be run multiple times. 
 -# Note that, at present, only one session of plda_run() should be executed in each
    database. This is because each plda_run() stores its temporary learning data inside
    the madlib.lda_corpus table, and multiple plda_run()'s will attempt to write to the
    same table and thus corrupt the intermediate learning result.  
 
-After a successful run of the plda_run() function, the results of learning can be
-obtained by running the following inside the Greenplum Database.
+After a successful run of the plda_run() function, the most probable words associated 
+with each topic are displayed. Other results of the learning process can be obtained 
+by running the following commands inside the Greenplum Database.
 
 -# The topic assignments for each document can be obtained as follows:
    \code
-	select id, (topics).topics from madlib.lda_corpus;
+   select id, (topics).topics from madlib.lda_corpus;
    \endcode
-
 -# The number of times words in each document were assigned to each topic can be obtained as follows:
    \code
-	select id, (topics).topic_d from madlib.lda_corpus;
+   select id, (topics).topic_d from madlib.lda_corpus;
    \endcode
-
 -# The number of times each word was assigned to a topic can be computed as follows:
    \code
-	select ss.i, madlib.wordTopicDistrn(gcounts,$numtopics,ss.i) from madlib.globalWordTopicCount, (select generate_series(1,$dictsize) i) as ss where mytimestamp = $num_iter ;
+   select ss.i, madlib.wordTopicDistrn(gcounts,$numtopics,ss.i) 
+   from madlib.globalWordTopicCount, 
+        (select generate_series(1,$dictsize) i) as ss 
+   where mytimestamp = $num_iter ;
    \endcode
-
+   where $numtopics is the number of topics used in the learning process, and 
+   $num_iter is the number of iterations ran in the learning process.
 -# The total number of words assigned to each topic can be computed as follows:
    \code
-	select sum((topics).topic_d) topic_sums from madlib.lda_corpus;
+   select sum((topics).topic_d) topic_sums from madlib.lda_corpus;
    \endcode
 
 @literature
+
+Here are some relevant references.
 
 [1] D.M. Blei, A.Y. Ng, M.I. Jordan, <em>Latent Dirichlet Allocation</em>, 
     Journal of Machine Learning Research, vol. 3, pp. 993-1022, 2003.
@@ -156,7 +168,9 @@ obtained by running the following inside the Greenplum Database.
 [4] http://en.wikipedia.org/wiki/Latent_Dirichlet_allocation
 */
 
+
 \i plda_drop.sql
+
 
 -- The topics_t data type store the assignment of topics to each word in a document,
 -- plus the distribution of those topics in the document.
@@ -167,6 +181,9 @@ CREATE TYPE madlib.topics_t AS (
 
 -- The table where the corpus of documents to be analysed sits
 CREATE TABLE madlib.lda_corpus ( id int4, contents int4[], topics madlib.topics_t ) DISTRIBUTED RANDOMLY; -- BY (id);
+
+-- The table where the corpus of test documents to be labelled sits
+CREATE TABLE madlib.lda_testcorpus ( id int4, contents int4[], topics madlib.topics_t ) DISTRIBUTED RANDOMLY; -- BY (id);
 
 -- The table for storing dictionary.
 CREATE TABLE madlib.lda_dict ( id int4, a text[] ) DISTRIBUTED RANDOMLY;
@@ -325,6 +342,9 @@ BEGIN
 	    RAISE NOTICE '  Done iteration %', iter;
    	END LOOP;
 
+	-- Remove the trailing null's in the topics array
+	UPDATE madlib.lda_corpus SET topics.topics = (topics).topics[1:array_upper(contents,1)];
+
 	RETURN init_iter + num_iter;
 END;
 $$ LANGUAGE plpgsql;
@@ -379,7 +399,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TYPE madlib.word_weight AS ( word text, prob float, wcount int4 );
 
--- Returns the Pr( word | topic )g
+-- Returns the most important words for each topic, base on Pr( word | topic ).
 CREATE OR REPLACE FUNCTION madlib.topicWordProb(num_topics int4, topic int4, ltimestamp int4) RETURNS SETOF madlib.word_weight AS $$
 DECLARE
 	glbcounts int4[];
@@ -448,6 +468,42 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- This function computes the topic assignments to words in a document given previously computed
+-- statistics from the training corpus. This function and the next are still being tested.
+CREATE OR REPLACE FUNCTION 
+madlib.labelDocument(doc int4[], global_count int4[], topic_counts int4[], num_topics int4, dsize int4, 
+		     alpha float, eta float)
+RETURNS madlib.topics_t AS $$
+DECLARE
+	ret madlib.topics_t;
+BEGIN
+	ret := madlib.randomTopics(array_upper(doc,1), num_topics);
+	FOR i in 1..20 LOOP
+	    ret := madlib.randomAssignTopic(doc,ret,global_count,topic_counts,num_topics,dsize,alpha,eta);
+    	END LOOP;
+	ret.topics := ret.topics[1:array_upper(doc,1)];
+	RETURN ret;
+END;
+$$ LANGUAGE plpgsql;
+
+-- This function computes the topic assignments to documents in a test corpus named madlib.lda_testcorpus
+CREATE OR REPLACE FUNCTION
+madlib.labelTestDocuments(ltimestep int4, num_topics int4, alpha float, eta float)
+RETURNS VOID AS $$
+DECLARE
+	glwcounts int4[];
+	topic_counts int4[];
+	dsize int4;
+BEGIN
+	dsize := madlib.dictsize();
+	SELECT INTO glwcounts gcounts FROM madlib.globalWordTopicCount WHERE mytimestamp = ltimestep;
+	SELECT INTO topic_counts SUM((topics).topic_d) FROM madlib.lda_corpus;
+
+	UPDATE madlib.lda_testcorpus 
+	SET topics = madlib.labelDocument(contents, glwcounts, topic_counts, num_topics, dsize,
+	    	     		          alpha, eta);
+END;
+$$ LANGUAGE plpgsql;
 
 -- This function takes a fitted LDA model and computes a predictive distribution for new words in 
 -- a document.
